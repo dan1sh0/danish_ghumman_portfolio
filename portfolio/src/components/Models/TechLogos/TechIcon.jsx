46a15b3c8c471485d9064359afe9857211bad571
Danish Ghumman <danishg522@gmail.com>
2025-06-18 23:55:35 -0400

    created jsx file for the 3d icons and rendered them

---
+++ portfolio/src/components/Models/TechLogos/TechIcon.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Environment } from '@react-three/drei'
+import { Float } from '@react-three/drei'
+
+// in this we will use the 3d models for the icons 
+// get the glb files for each icon 
+// add lighting 
+// apply environement reflections 
+// wrap the model in floating animation 
+// disable zoom using orbit controls 
+
+const TechIcon = ({ model }) => {
+
+    const scene = useGLTF(model.modelPath);
+
+    return (
+        // canvas is the main container for the 3d model 
+        // ambient light is the light that is always on 
+        // environment preset is the environment that is applied to the model 
+        // float is the floating animation that is applied to the model 
+        // group is the container for the model 
+        // primitive is the model that is applied to the group 
+
+        <Canvas>
+            <ambientLight intensity={0.5} />
+            <Environment preset="city" />
+            <Float speed={5.5} rotationIntensity={0.5} floatIntensity={2}>
+                {/* scale is the size of the model */}
+                {/* rotation is the rotation of the model */}
+                {/* these can be changed individually for each icon in index.js */}
+                <group scale={model.scale} rotation={model.rotation}>
+                    <primitive object={scene.scene} />
+                </group>
+            </Float>
+        </Canvas>
+    )
+}
+
+export default TechIcon
